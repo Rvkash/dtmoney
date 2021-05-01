76c2dda3bee4a68eac7b98c445fa0e33dbe6c232
Rvkash <rafael.ieel@hotmail.com>
2021-05-01 17:44:37 -0300

    feat: modal and user click

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,43 @@
+import { useState } from 'react';
+import Modal from 'react-modal'
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header'
 import { GlobalStyle } from './styles/global';
 
+//Accessibility =>
+Modal.setAppElement('#root')
+
 export function App() {
+  const [
+    isNewTransactionModalOpen, 
+    setisNewTransactionModalOpen] 
+    = 
+    useState(false)
+
+  // Handle = User Click => 
+  function handleOpenNewTransactionModal() {
+    setisNewTransactionModalOpen(true)
+  }
+
+  function handleCloseNewTransactionModal() {
+    setisNewTransactionModalOpen(false)
+  }
+
   return (
   <>
-    <Header />
+    <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
+
     <Dashboard />
+
+    <Modal 
+      isOpen={isNewTransactionModalOpen}
+      onRequestClose={handleCloseNewTransactionModal}
+      >
+      <h2>Cadastrar transação</h2>
+    </Modal>
+
     <GlobalStyle />
+
   </>
   )
 }
