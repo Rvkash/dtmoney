a1eaf5c679b8a8ba0c658b7fb12cbeb236c8d5f3
Rvkash <rafael.ieel@hotmail.com>
2021-05-09 14:44:09 -0300

    feat: onRequestClose function

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import Modal from 'react-modal'
 import { Dashboard } from './components/Dashboard';
 import { Header } from './components/Header'
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from './styles/global';
 
 //Accessibility =>
@@ -26,16 +27,11 @@ export function App() {
   return (
   <>
     <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
-
     <Dashboard />
-
-    <Modal 
-      isOpen={isNewTransactionModalOpen}
-      onRequestClose={handleCloseNewTransactionModal}
-      >
-      <h2>Cadastrar transação</h2>
-    </Modal>
-
+    <NewTransactionModal 
+     isOpen={isNewTransactionModalOpen}
+     onRequestClose={handleCloseNewTransactionModal} 
+    />
     <GlobalStyle />
 
   </>
